eddea683c14f39c1e5b271148fa7cd9f7bb760f3
yyxxh <yyxxh@qq.com>
2018-05-18 19:16:44 +0800

    1

--- login.php
+++ login.php
@@ -3,10 +3,9 @@ require_once 'conn.php';
 $user=$_POST['uname'];
 $psw=$_POST['psw'];
 $sql="select * from user where uname='$user'";
-//$row=$dbh->query($sql);
-foreach ($dbh->query($sql) as $row) {
-    print $row['uname'] . "\t";
-}
-/*
-if($row['password']!=$psw)
-  echo '密码错误！';*/
+$sth=$dbh->query($sql);
+$row=$sth->fetch();
+echo $row[1];
+if($row[2]!=$psw)
+  echo '密码错误！';
+ 
